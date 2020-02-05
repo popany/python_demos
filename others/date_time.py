afeadf953b169b7ebfadada7d4be182ba475987f
popany <popany@163.com>
2020-02-05 14:40:54 +0000

    mod

--- others/date_time.py
+++ others/date_time.py
@@ -1,9 +1,11 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 d1 = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
 d2 = datetime.strptime('[2020/01/29 00:03:09,669]', '[%Y/%m/%d %H:%M:%S,%f]')
 d3 = datetime(2020, 1, 2)
+d4 = d3 - timedelta(days=1)
 
 print(d1.strftime("%Y-%m-%d %H:%M:%S.%f"))
 print(d2.strftime("%Y-%m-%d %H:%M:%S.%f"))
 print(d3.strftime("%Y-%m-%d %H:%M:%S.%f"))
+print(d4.strftime("%Y-%m-%d %H:%M:%S.%f"))
