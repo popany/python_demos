28c518fd642c1f110d85c249660b739455c65a0e
popany <popany@163.com>
2020-02-05 16:30:08 +0000

    mod

--- others/datetime.py
+++
@@ -1,4 +0,0 @@
-from datetime import datetime
-
-datetime_1 = datetime.strptime('Jun 1 2005  1:33PM', '%b %d %Y %I:%M%p')
-datetime_2 = datetime.strptime('[2020/01/29 00:03:09,669]', '[%Y/%m/%d %H:%M:%S,%f]')
